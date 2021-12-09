b88fcf29eab245f192720f142343d429fa58787b
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-12-09 16:14:38 +0300

    Delete OpenStart directory

--- OpenStart/main.js
+++
@@ -1,9 +0,0 @@
-const messages = document.querySelectorAll( '.js-message')
-
-if (messages) {
-  messages.forEach(message => {
-    message.addEventListener('click', (evt) => {
-      alert('Hello, world!')
-    })
-  })
-}
